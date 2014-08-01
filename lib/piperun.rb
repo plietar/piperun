9786cd440fee56ee83e47b7315c1a85b7ae0aaa1
Paul Liétar <paul@lietar.net>
2014-08-01 17:03:27 +0200

    Replace mercenary by methadone.

--- lib/piperun.rb
+++ lib/piperun.rb
@@ -3,6 +3,9 @@ require 'piperun/pipeline'
 require 'piperun/filter'
 require 'piperun/version'
 
+require 'methadone'
+
 module Piperun
+  include Methadone::CLILogging
 end
 
---
+++ lib/piperun/cli.rb
@@ -0,0 +1,36 @@
+require 'piperun'
+require 'methadone'
+
+module Piperun
+  class CLI
+    include Methadone::Main
+    include Methadone::CLILogging
+
+    description "Process files with pipelines"
+    version Piperun::VERSION
+
+    on "-V", "--verbose", "Run verbosely"
+    on "--watch", ""
+
+    main do
+      if options[:verbose]
+        Piperun::logger.level = Logger::DEBUG
+      else
+        Piperun::logger.level = Logger::INFO
+      end
+
+      network = Piperun::Network.load "Pipeline.rb"
+
+      unless options[:watch]
+        network.run
+      else
+        network.watch
+        begin
+          sleep
+        rescue Interrupt
+        end
+      end
+    end
+  end
+end
+
