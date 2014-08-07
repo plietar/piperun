9151e5cee6eb8ca1d53f3d0dc920faaa5b4ad30f
Paul Liétar <paul@lietar.net>
2014-08-07 21:57:46 +0200

    Improve typescript filter.
    The filter now has two modes, one takes individual files and compiles them
    to javascript. The second compiles all files at once and outputs a single
    javascript file.

--- lib/piperun/filters/typescript_filter.rb
+++ lib/piperun/filters/typescript_filter.rb
@@ -1,9 +1,49 @@
 module Piperun::Filters
-  class TypeScriptFilter < TransformFilter
-    replace_extension '.js'
+  class TypeScriptFilter < Filter
+    def initialize(options = {})
+      super()
+      @options = {
+        output: nil,
+        noImplicitAny: false
+      }.merge(options)
+    end
 
-    def transform(src, dst)
-      unless system("tsc #{src} --out #{dst}")
+    def run(src, dst, files)
+      argv = ["tsc"]
+      argv << "--noImplicitAny" if @options[:noImplicitAny]
+
+      unless @options[:output].nil?
+        files.map! { |f| File.join(src, f) }
+        dstpath = File.join(dst, @options[:output])
+
+        FileUtils::mkdir_p File.dirname(dstpath)
+        tsc *argv, "--out", dstpath, *files
+
+        [@options[:output]]
+      else
+        outfiles = []
+
+        files.each do |name|
+          outname = "#{File.join(File.dirname(name), File.basename(name, '.*'))}.js"
+
+          srcpath = File.join(src, name)
+          dstpath = File.join(dst, outname)
+          FileUtils::mkdir_p File.dirname(dstpath)
+
+          tsc *argv, "--outDir", File.dirname(dstpath), srcpath
+
+          outfiles << outname
+        end
+
+        outfiles
+      end
+    end
+
+    private
+    def tsc(*argv)
+      pid = Process.spawn(*argv)
+      Process.wait(pid)
+      unless $?.exitstatus == 0
         raise StandardError, "Typescript Compiler failed"
       end
     end
